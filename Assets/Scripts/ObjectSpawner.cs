eedd3c5a5aeb37856a52023c987926b03b97ae91
questroo <42195958+questroo@users.noreply.github.com>
2020-09-15 21:30:20 -0700

    - Added PlacementIndicator which displays what plane the AR camera is looking at. - Added Object Spawner which spawns cube at the PlacementIndicators position when the screen is touched.

---
+++ Assets/Scripts/ObjectSpawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSpawner : MonoBehaviour
+{
+    public GameObject objectToSpawn;
+    private PlacementIndicator placementIndicator;
+
+    private void Start()
+    {
+        placementIndicator = FindObjectOfType<PlacementIndicator>();
+    }
+
+    private void Update()
+    {
+        if(Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began)
+        {
+            SpawnObject();
+        }
+    }
+
+    private void SpawnObject()
+    {
+        GameObject obj = Instantiate(objectToSpawn,
+            placementIndicator.transform.position,
+            placementIndicator.transform.rotation);
+    }
+}
---
+++ Assets/Scripts/PlacementIndicator.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class PlacementIndicator : MonoBehaviour
+{
+    private ARRaycastManager rayManager;
+    private GameObject visual;
+
+    private void Start()
+    {
+        // Get the components
+        rayManager = FindObjectOfType<ARRaycastManager>();
+        visual = transform.GetChild(0).gameObject;
+
+        // Hide the placement visual
+        visual.SetActive(false);
+    }
+
+    private void Update()
+    {
+        // Shoot a raycast from the center of the screen
+        List<ARRaycastHit> hits = new List<ARRaycastHit>();
+        rayManager.Raycast(new Vector2(Screen.width / 2, Screen.height / 2), hits, TrackableType.Planes);
+
+        // If we hit an AR plane, update the position and rotation
+        if(hits.Count > 0)
+        {
+            transform.position = hits[0].pose.position;
+            transform.rotation = hits[0].pose.rotation;
+
+            if(!visual.activeInHierarchy)
+            {
+                visual.SetActive(true);
+            }
+        }
+    }
+}
